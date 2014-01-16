d7809cae86aa3144b6825d0573408d9a09fe8823
frankhuang00727 <frankjc27@yahoo.com>
2014-01-16 01:37:24 -0500

    Update Prototype
    Make the application look like the fluid UI

---
+++ RosePerks/src/edu/rosehulman/roseperks/MainScreenActivity.java
@@ -0,0 +1,48 @@
+package edu.rosehulman.roseperks;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+
+public class MainScreenActivity extends Activity {
+	private Button PerksList;
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main_screen);
+		PerksList = (Button) findViewById(R.id.AllPerks_Button);
+		PerksList.setOnClickListener(new OnClickListener() {
+
+			@Override
+			public void onClick(View v) {
+				Intent DetailView = new Intent(getApplicationContext(),
+						PerksListActivity.class);
+				startActivity(DetailView);
+			}
+		});
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.main_screen, menu);
+		return true;
+	}
+
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		switch (item.getItemId()) {
+		case R.id.action_settings:
+			finish();
+			return true;
+
+		}
+		return super.onOptionsItemSelected(item);
+	}
+}
